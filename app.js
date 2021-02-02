09abeea3a0127c4abb00dac69ec5d20654844546
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-02-02 14:12:52 -0600

    added inquirer to project

--- app.js
+++ app.js
@@ -1,64 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const [name, github] = profileDataArgs;
-//   // const name = profileDataArgs[0];
-//   // const github = profileDataArgs[1];
-
+const inquirer = require('inquirer');
 // const fs = require('fs');
-// const generatePage = (name, github) => {
-//   return `
-//   <!DOCTYPE html> 
-//   <html lang="en"> 
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Portfolio Demo</title>
-//   </head>
+// const generatePage = require('./src/page-template');
 
-//   <body>
-//     <h1>${name}</h1>
-//     <h2><a href="https://github.com/${github}">Github</a></h2>
-//   </body>
-//   </html>
-//   `;
-// };
+// const pageHTML = generatePage(name, github);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-// console.log(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
+// fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-// console.log(profileDataArgs);
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-//   };
-
-//   printProfileData(profileDataArgs);
